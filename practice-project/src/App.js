dc6868a9bd4065ec72673880aa8c98a27048b0ed
Vishnu Prasanna <67537099+vishnusai14@users.noreply.github.com>
2022-02-20 11:05:26 +0530

    practice-project-complete

---
+++ practice-project/src/App.js
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //This will Create an Infinte Loop
+  // if(localStorage.getItem("isLoggedIn") === "1") {
+  //   setIsLoggedIn(true)
+  // }
+
+  //if we leave the dependencies as empty then it will execute for first time == > componentDidMount
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn") === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.setItem("isLoggedIn", "0");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
